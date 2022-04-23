efecd6547b81379cacf6ad44af347a006a217751
Asif Sajjad <asifrps17@gmail.com>
2022-04-23 11:46:09 +0530

    Update the forked code and added basic functionalities
    index.html, script.js and style.css is updated to include the basic function of the game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameContainer = document.getElementsById("game");
+const gameContainer = document.getElementById("game");
 
 const COLORS = [
   "red",
@@ -56,12 +56,69 @@ function createDivsForColors(colorArray) {
     gameContainer.append(newDiv);
   }
 }
-
+setTimeout(()=>{
+  const elements=document.querySelectorAll("#game > div");
+  for(let element of elements){
+    element.setAttribute("style","background-color:gray");
+  }
+},2000);
+let lastClickedImage=""
 // TODO: Implement this function!
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
+  const clickedCard= event.target.classList[0];
+  console.log(clickedCard);
+  if(!lastClickedImage){
+    lastClickedImage=clickedCard;
+    event.target.setAttribute("style",`background-color:${clickedCard}`);
+  }
+  else if(clickedCard!==lastClickedImage){
+    event.target.setAttribute("style",`background-color:${clickedCard}`);
+    gameContainer.setAttribute("style","pointer-events:none");
+    const myPromise= new Promise((res,rej)=>{setTimeout(()=>{event.target.setAttribute("style","background-color:gray");
+  res();},1000)});
+  myPromise.then(gameContainer.setAttribute("style","pointer-events:auto"));
+    
+  }
+  else{
+    lastClickedImage="";
+    event.target.setAttribute("style",`background-color:${clickedCard}`);
+  }
+  
   console.log("you clicked",event.target);
 }
 
 // when the DOM loads
 createDivsForColors(shuffledColors);
+
+// ...To be used in bonus task..... 
+
+// setTimeout(()=>{
+//   const elements=document.querySelectorAll("#game > div");
+//   for(let element of elements){
+//     element.setAttribute("style","background-color:black;background-image:none");
+//   }
+// },2000);
+// let lastClickedImage=""
+// // TODO: Implement this function!
+// function handleCardClick(event) {
+//   // you can use event.target to see which element was clicked
+//   const clickedCard= event.target.classList[0];
+//   const clickedCardGif= COLORS.indexOf(clickedCard) + 1;
+//   console.log(clickedCard,clickedCardGif);
+//   if(!lastClickedImage){
+//     lastClickedImage=clickedCard;
+//     event.target.setAttribute("style",`background-image:./gifs/${clickedCardGif}.gif`);
+//   }
+//   else if(clickedCard!==lastClickedImage){
+//     event.target.setAttribute("style",`background-image:./gifs/${clickedCardGif}.gif`);
+//     setTimeout(()=>{event.target.setAttribute("style","background-color:black;background-image:none");},2000);
+    
+//   }
+//   else{
+//     lastClickedImage="";
+//     event.target.setAttribute("style",`background-image:./gifs/${clickedCardGif}.gif`);
+//   }
+  
+//   console.log("you clicked",event.target);
+// }
